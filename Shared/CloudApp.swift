520c4615102b62b39812ba39e09d4587916d0938
Jefry <jefry.eko@ovo.id>
2023-01-13 21:35:06 +0700

    initial commit

---
+++ Shared/CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudApp.swift
+//  Shared
+//
+//  Created by Jefry Eko on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
